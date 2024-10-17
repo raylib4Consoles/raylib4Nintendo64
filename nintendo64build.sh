993f98a299c4044ae85bcdaec9561689925009ab
Antonio Jose Ramos Marquez <psxdev@gmail.com>
2024-10-17 18:26:33 +0200

    Update nintendo64build.sh

--- nintendo64build.sh
+++ nintendo64build.sh
@@ -3,7 +3,7 @@
 if test ! $N64_INST; then { echo "ERROR: Set \$N64_INST installing libdragon before continuing."; exit 1; } fi
 
 ## We are using dreamcast branch including N64 changes 
-git clone https://github.com/raylib4Consoles/raylib -b dreamcast
+git clone https://github.com/raylib4Consoles/raylib -b raylib4Consoles
 ## Copy file needed to build raylib
 cp custom/raylib4Nintendo64.mk $N64_INST/include
 cd raylib/src
